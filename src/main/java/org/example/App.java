83d1367eb0ad7cc28875920a86ccdad7c3517dbf
Reese <reesespector@knights.ucf.edu>
2021-09-08 10:12:12 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // scanner
+        Scanner input = new Scanner(System.in);
+
+        // take number one
+        System.out.print("What is the first number? ");
+        String strOne = input.nextLine();
+        int numOne = Integer.parseInt(strOne);
+
+        // take number two
+        System.out.print("What is the second number? ");
+        String strTwo = input.nextLine();
+        int numTwo = Integer.parseInt(strTwo);
+
+        //print
+        System.out.print(
+            //sum
+                numOne + " + " + numTwo + " = " + (numOne + numTwo) + "\n" +
+            //dif
+                numOne + " - " + numTwo + " = " + (numOne - numTwo) + "\n" +
+            //prod
+                numOne + " * " + numTwo + " = " + (numOne * numTwo) + "\n" +
+            //quo
+                numOne + " / " + numTwo + " = " + (numOne / numTwo) + "\n"
+        );
+    }
+}
